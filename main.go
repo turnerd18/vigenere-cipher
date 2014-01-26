9ef27a40bc8dffb46cf85bf60e5ed92e81a6af76
turnerd18 <turnerd18@gmail.com>
2014-01-25 23:46:31 -0600

    encryption & decryption working

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	//"strconv"
 	"strings"
+)
+
+const (
+	ALPHABET_LENGTH int = 26
+	LETTERS string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 )
 
 var (
@@ -13,14 +17,37 @@ var (
 	decrypt = flag.Bool("decrypt", false, "decrypt input with key instead of encrypt")
 )
 
-func vigenereCipher(input string, key string, decrypt bool) (string, error) {
-	return "", nil
+func vigenereCipher(input string, key string, decrypt bool) string {
+	chars := []rune(input)
+	k := []rune(key)
+	keyPos := 0
+	output := ""
+	for _, m := range chars {
+		index := int(m - 'A')
+		offset := int(k[keyPos] - 'A') % ALPHABET_LENGTH
+		if decrypt {
+			index -= offset
+		} else {
+			index += offset
+		}
+		if index >= ALPHABET_LENGTH {
+			index -= ALPHABET_LENGTH
+		} else if index < 0 {
+			index += ALPHABET_LENGTH
+		}
+		output += string(LETTERS[index])
+
+		keyPos++
+		if keyPos == len(key) {
+			keyPos = 0
+		}
+	}
+	return output
 }
 
 func main() {
 	flag.Parse()
 	input := strings.ToUpper(strings.Replace(*input, " ", "", -1))
 	key := strings.ToUpper(strings.Replace(*key, " ", "", -1))
-	fmt.Printf("%v %v\n", input, key)
-	fmt.Printf("%d\n", 's' - 'a')
+	fmt.Printf("%v\n", vigenereCipher(input, key, *decrypt))
 }
